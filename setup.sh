9e424ffa7aa4530377a33fb55baf2ad722d3b5d6
Alexander Smolyanskiy <asmoly@outlook.com>
2026-07-12 21:36:29 -0700

    updated the setup script to also install cuda

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@ conda tos accept
 conda create -n centerpoint python=3.8 -y
 conda activate centerpoint
 
+conda install -c "nvidia/label/cuda-11.6.2" cuda-toolkit -y
+
+export CUDA_HOME=$CONDA_PREFIX
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+which nvcc
+nvcc --version
+
 nvidia-smi
 
 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 \
