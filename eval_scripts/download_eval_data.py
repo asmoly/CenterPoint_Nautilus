37107f3196dd630ff92da215d0d65f7798778e8b
Alexander Smolyanskiy <asmoly@outlook.com>
2026-07-26 12:41:20 +0300

    Added extraction to download function as well

--- eval_scripts/download_eval_data.py
+++ eval_scripts/download_eval_data.py
@@ -1,21 +1,35 @@
+import zipfile
 import requests
 from pathlib import Path
 
-def download_data(out_path="v2x_real_lidar64_val.zip"):
+def download_data():
     url = "https://ucla.app.box.com/shared/static/est8t7lxirg85ohkgoueietxd0xqpf36"
-    out = Path(out_path)
 
-    if out.exists():
-        print("ERROR: PATH ALREADY EXISTS")
-        return
+    zip_path = Path("v2x_real_lidar64_val.zip")
+    out_dir = Path("v2x_real_lidar64_val")
 
-    with requests.get(url, stream=True, allow_redirects=True) as r:
-        r.raise_for_status()
-        with out.open("wb") as f:
-            for chunk in r.iter_content(chunk_size=1024*1024):
-                if chunk:
-                    f.write(chunk)
+    if zip_path.exists():
+        print("ERROR: DOWNLOADED ZIP ALREADY EXISTS")
+    else:
+        with requests.get(url, stream=True, allow_redirects=True) as r:
+            r.raise_for_status()
+            with zip_path.open("wb") as f:
+                for chunk in r.iter_content(chunk_size=1024*1024):
+                    if chunk:
+                        f.write(chunk)
 
-    print(f"Downloaded {out} ({out.stat().st_size / 1e9:.2f} GB)")
+        print(f"Downloaded {zip_path} ({zip_path.stat().st_size / 1e9:.2f} GB)")
+
+    if out_dir.exists():
+        print("ERROR: EXTRACTED FILES ALREADY EXIST")
+    else:
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, "r") as zf:
+            zf.extractall(out_dir)
+
+        print(f"Extracted to {out_dir}")
+
+    print("Finished downloading and extracting")
 
 download_data()
