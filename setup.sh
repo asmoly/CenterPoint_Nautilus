641888991b117f5956484075023952532f21a5ea
Alexander Smolyanskiy <asmoly@outlook.com>
2026-07-28 13:17:54 +0500

    Brought back accidentally deleted file and renamed it

--- setup.sh
+++ setup.sh
@@ -30,5 +30,5 @@ python -c "import pcdet.ops.iou3d_nms.iou3d_nms_cuda as iou3d; print('iou3d ok')
 git clone https://github.com/DerrickXuNu/OpenCOOD.git
 cd OpenCOOD
 pip install -r requirements.txt
-python setup.py develop
+python setup_pcdet.py develop
 cd ..
